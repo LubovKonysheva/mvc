98fe676f61fca8eb4e80c778445ea4203bc21c73
lyubov <konysheva.lv@yandex.ru>
2024-02-11 12:40:25 +0300

    3 неделя

---
+++ base/config.php
@@ -0,0 +1,13 @@
+<?php
+const DB_USER = 'luba';
+const DB_NAME = 'mvc';
+const DB_HOST = 'localhost';
+const DB_PASSWORD = 'root';
+
+const ADMIN_IDS = [9];
+
+function d(...$args)
+{
+    var_dump($args);
+    die;
+}
